c1c07fa37155810c5ad186abc9a38dbbcd31d9ee
Gabriel Reyes <gareyes@stanford.edu>
2021-09-26 12:30:10 -0700

    found the sum of 2 variables

--- pset0.R
+++ pset0.R
@@ -1,1 +1,14 @@
-print("hello world")
+# Author: Gabriel Reyes
+# Class: PSYCH 251
+# Assignment: PSET #1
+# Instructor: Michael Frank
+
+################################## 
+###### CREATING RANDOM CODE ######
+################################## 
+
+x <- 4
+y <- 8
+
+sum <- x + y
+print(sum)
